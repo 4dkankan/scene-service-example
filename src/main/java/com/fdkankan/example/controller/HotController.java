d9b399f2d8796fdce999c899ae16027f26a8cc2a
dengsixing <879865044@qq.com>
2024-01-27 15:03:15 +0800

    数据写入目录指定配置

--- src/main/java/com/fdkankan/example/controller/HotController.java
+++ src/main/java/com/fdkankan/example/controller/HotController.java
@@ -1,10 +1,11 @@
 package com.fdkankan.example.controller;
 
+import cn.hutool.core.io.FileUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.fdkankan.example.bean.ResultData;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("hot")
@@ -12,13 +13,29 @@ public class HotController {
 
     private static final String HOTJSON_FILENAME_FORMAT = "hot-%s.json";
 
-    @Value("path.data.hot.path")
+    @Value("${path.data.hot.path}")
     private String hotPath;
 
     @GetMapping
     public ResultData get(String num){
+
         String hotJsonFile = String.format(HOTJSON_FILENAME_FORMAT, num);
-        return ResultData.ok("hahaha");
+        String hotJsonFilePath = hotPath.concat(hotJsonFile);
+        if(!FileUtil.exist(hotJsonFilePath)){
+            return ResultData.ok();
+        }
+        String jsonStr = FileUtil.readUtf8String(hotJsonFilePath);
+        return ResultData.ok(JSON.parseObject(jsonStr));
     }
+
+    @PostMapping
+    public ResultData save(String num, @RequestBody JSONObject hotJson){
+        String hotJsonFile = String.format(HOTJSON_FILENAME_FORMAT, num);
+        String hotJsonFilePath = hotPath.concat(hotJsonFile);
+        FileUtil.mkParentDirs(hotJsonFilePath);
+        FileUtil.writeUtf8String(hotJson.toJSONString(), hotJsonFilePath);
+        return ResultData.ok();
+    }
+
 
 }
